d1d52a64156df031ad479dafd2fd05791117c371
Ashutosh Anand <131291771+ashutosh28102018@users.noreply.github.com>
2024-06-05 11:53:57 +0530

    Update addEmployeeEmail.js

--- mailers/addEmployeeEmail.js
+++ mailers/addEmployeeEmail.js
@@ -3,7 +3,7 @@ const nodeMailer=require('../config/nodemailer');
 module.exports.joiningEmail=(user)=>{
     console.log('user details',user);
     nodeMailer.transporter.sendMail({
-        from:'garimaj334@gmail.com',
+        from:'ashutoshanand28102018@gmail.com',
         to:user.email,
         subject:"Welcome to Coding Ninjas",
         html:`<h1>we are very excited to welcome you in our company ${user.name}</h1><h3> you can login to our company employee page with your credentials as </h3>
